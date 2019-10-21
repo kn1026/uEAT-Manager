9d617f83783dc9b0dd30a542b92075e52e506ba8
Cracc team <31965340+Craccteam@users.noreply.github.com>
2019-10-21 04:25:02 -0400

    Initial Commit

---
+++ uEAT ManagerTests/uEAT_ManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  uEAT_ManagerTests.swift
+//  uEAT ManagerTests
+//
+//  Created by Khoi Nguyen on 10/21/19.
+//  Copyright © 2019 Khoi Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import uEAT_Manager
+
+class uEAT_ManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
